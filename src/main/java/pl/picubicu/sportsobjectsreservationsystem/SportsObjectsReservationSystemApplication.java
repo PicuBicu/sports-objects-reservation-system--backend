981a3ae2e9d85a0dafae7dbfd57210331f12c5b5
PicuBicu <championello@gmail.com>
2022-03-12 16:58:59 +0100

    Initial commit

---
+++ src/main/java/pl/picubicu/sportsobjectsreservationsystem/SportsObjectsReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.picubicu.sportsobjectsreservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsObjectsReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsObjectsReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/picubicu/sportsobjectsreservationsystem/SportsObjectsReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.picubicu.sportsobjectsreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsObjectsReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
