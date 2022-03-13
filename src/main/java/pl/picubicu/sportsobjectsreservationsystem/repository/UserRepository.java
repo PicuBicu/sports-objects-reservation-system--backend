628628090783004804fbe599d51c77074291d6d2
PicuBicu <championello@gmail.com>
2022-03-13 14:23:58 +0100

    Add missing repository annotation

--- src/main/java/pl/picubicu/sportsobjectsreservationsystem/repository/UserRepository.java
+++ src/main/java/pl/picubicu/sportsobjectsreservationsystem/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package pl.picubicu.sportsobjectsreservationsystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.picubicu.sportsobjectsreservationsystem.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
