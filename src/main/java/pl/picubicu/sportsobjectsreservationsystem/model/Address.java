f2e5f8486c44d713db3f6ff02849251975ecf967
PicuBicu <championello@gmail.com>
2022-03-26 14:58:50 +0100

    Add static factory method in address model

--- src/main/java/pl/picubicu/sportsobjectsreservationsystem/model/Address.java
+++ src/main/java/pl/picubicu/sportsobjectsreservationsystem/model/Address.java
@@ -1,8 +1,10 @@
 package pl.picubicu.sportsobjectsreservationsystem.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import pl.picubicu.sportsobjectsreservationsystem.dto.RegistrationRequestDto;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,11 +12,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "addresses")
+@Table(name = "address")
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,4 +26,15 @@ public class Address {
     private String streetName;
     private String streetNumber;
     private String localNumber;
+    private String cityName;
+
+    public static Address fromRegistrationDto(RegistrationRequestDto request) {
+        return Address.builder()
+                .streetName(request.getStreetName())
+                .streetNumber(request.getStreetNumber())
+                .localNumber(request.getLocalNumber())
+                .cityName(request.getCityName())
+                .build();
+
+    }
 }
