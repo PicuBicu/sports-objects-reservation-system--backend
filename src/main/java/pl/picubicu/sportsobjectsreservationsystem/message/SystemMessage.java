60c0fadce511805f20bc26ccdbd248627b15962b
PicuBicu <championello@gmail.com>
2022-03-26 14:57:17 +0100

    Add class representing system response messages for user registration validation

---
+++ src/main/java/pl/picubicu/sportsobjectsreservationsystem/message/SystemMessage.java
@@ -0,0 +1,7 @@
+package pl.picubicu.sportsobjectsreservationsystem.message;
+
+public class SystemMessage {
+    public final static String USER_ALREADY_EXISTS = "User already exist";
+    public final static String USER_CREATED = "User has been created successfully";
+    public final static String USER_NOT_ACTIVATED = "This account has not yet been activated";
+}
