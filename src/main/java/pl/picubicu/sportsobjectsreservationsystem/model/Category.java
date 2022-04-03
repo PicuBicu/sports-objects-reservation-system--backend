177fd8dd08e689a274c442560576e0d03885544e
PicuBicu <championello@gmail.com>
2022-04-03 23:39:33 +0200

    Add unique to name in category model

--- src/main/java/pl/picubicu/sportsobjectsreservationsystem/model/Category.java
+++ src/main/java/pl/picubicu/sportsobjectsreservationsystem/model/Category.java
@@ -1,15 +1,30 @@
 package pl.picubicu.sportsobjectsreservationsystem.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
-@Entity
+@NoArgsConstructor
+@Data
+@Entity(name = "Category")
+@Table(name = "categories",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "category_name_unique_constraint",
+                        columnNames = {
+                                "name"
+                        })
+        })
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 }
