4544c1c73232dd3606b7c0fcc719aefd7109ca4c
PicuBicu <championello@gmail.com>
2022-04-24 18:32:36 +0200

    Update reservation model

--- src/main/java/pl/picubicu/sportsobjectsreservationsystem/reservation/Reservation.java
+++ src/main/java/pl/picubicu/sportsobjectsreservationsystem/reservation/Reservation.java
@@ -1,43 +1,52 @@
 package pl.picubicu.sportsobjectsreservationsystem.reservation;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 import pl.picubicu.sportsobjectsreservationsystem.sportobject.SportObject;
 import pl.picubicu.sportsobjectsreservationsystem.user.User;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import java.time.Instant;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@Entity(name = "Reservation")
+@Table(name = "reservations")
 public class Reservation {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne()
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
     private User user;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.EAGER)
     private ReservationStatus status;
 
-    private Instant date;
+    @CreationTimestamp
+    private Instant creationDate;
+
+    private Instant reservationDate;
 
     private Integer numOfUsers;
 
-    @ManyToOne()
-    @JoinColumn(name = "sport_object_id", referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sport_object_id", referencedColumnName = "id", nullable = false)
     private SportObject sportObject;
 
 }
