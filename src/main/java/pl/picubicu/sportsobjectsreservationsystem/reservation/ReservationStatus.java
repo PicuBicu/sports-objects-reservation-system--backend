fa878dddaf2b0870c8f9e66c6a68e788650ac89a
PicuBicu <championello@gmail.com>
2022-04-24 18:33:36 +0200

    Add unique constraint in reservation statuses model

--- src/main/java/pl/picubicu/sportsobjectsreservationsystem/reservation/ReservationStatus.java
+++ src/main/java/pl/picubicu/sportsobjectsreservationsystem/reservation/ReservationStatus.java
@@ -1,22 +1,36 @@
 package pl.picubicu.sportsobjectsreservationsystem.reservation;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
-@Entity
+@Entity(name = "ReservationStatus")
+@Table(name = "reservation_statuses", uniqueConstraints = {
+        @UniqueConstraint(name = "status_name_unique_constraint",
+                columnNames = {
+                        "name"
+                })
+})
 public class ReservationStatus {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
+    public ReservationStatus(String statusName) {
+        name = statusName;
+    }
 }
