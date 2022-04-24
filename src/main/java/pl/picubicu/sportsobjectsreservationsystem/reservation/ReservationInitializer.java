6e294d1608034bbe8611501b6781a008066e6ed2
PicuBicu <championello@gmail.com>
2022-04-24 18:34:30 +0200

    Add reservation status repository and initialized it with statuses values before application starts

---
+++ src/main/java/pl/picubicu/sportsobjectsreservationsystem/reservation/ReservationInitializer.java
@@ -0,0 +1,22 @@
+package pl.picubicu.sportsobjectsreservationsystem.reservation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class ReservationInitializer implements CommandLineRunner {
+
+    private final ReservationStatusRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.save(new ReservationStatus("REQUESTED"));
+        repository.save(new ReservationStatus("ACCEPTED"));
+        repository.save(new ReservationStatus("CANCELED"));
+        repository.save(new ReservationStatus("APPROVED"));
+        repository.save(new ReservationStatus("FINALIZED"));
+    }
+
+}
