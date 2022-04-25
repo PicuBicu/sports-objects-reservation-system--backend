e7ca964be5b594764df097a2d7301c16e6f091f8
PicuBicu <championello@gmail.com>
2022-04-25 20:41:21 +0200

    Change reservation statuses names

--- src/main/java/pl/picubicu/sportsobjectsreservationsystem/reservation/ReservationInitializer.java
+++ src/main/java/pl/picubicu/sportsobjectsreservationsystem/reservation/ReservationInitializer.java
@@ -13,9 +13,9 @@ public class ReservationInitializer implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         repository.save(new ReservationStatus("REQUESTED"));
-        repository.save(new ReservationStatus("ACCEPTED"));
-        repository.save(new ReservationStatus("CANCELED"));
         repository.save(new ReservationStatus("APPROVED"));
+        repository.save(new ReservationStatus("CANCELED"));
+        repository.save(new ReservationStatus("CONFIRMED"));
         repository.save(new ReservationStatus("FINALIZED"));
     }
 
