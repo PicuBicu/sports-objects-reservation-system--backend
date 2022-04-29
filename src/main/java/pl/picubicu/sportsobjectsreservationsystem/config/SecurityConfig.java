00aad53426a6e058214a0c8b654c4c01e3b33d82
PicuBicu <championello@gmail.com>
2022-04-29 20:47:21 +0200

    Make password encoder bean public

--- src/main/java/pl/picubicu/sportsobjectsreservationsystem/config/SecurityConfig.java
+++ src/main/java/pl/picubicu/sportsobjectsreservationsystem/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
     }
 }
