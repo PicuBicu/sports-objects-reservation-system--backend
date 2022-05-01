07841ecfd8f5feabb325512b3dfe668e88b28004
PicuBicu <championello@gmail.com>
2022-05-01 18:11:38 +0200

    Map roles collection in user response dto

--- src/main/java/pl/picubicu/sportsobjectsreservationsystem/user/UserResponseDto.java
+++ src/main/java/pl/picubicu/sportsobjectsreservationsystem/user/UserResponseDto.java
@@ -3,16 +3,21 @@ package pl.picubicu.sportsobjectsreservationsystem.user;
 import lombok.Builder;
 import lombok.Data;
 import pl.picubicu.sportsobjectsreservationsystem.address.Address;
+import pl.picubicu.sportsobjectsreservationsystem.user.role.Role;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Builder
 @Data
 public class UserResponseDto {
 
+    private String jwtToken;
     private String firstName;
     private String lastName;
     private String phoneNumber;
     private String email;
-    private String role;
+    private Collection<String> role;
     private Address address;
 
     public static UserResponseDto fromUser(User user) {
@@ -21,7 +26,10 @@ public class UserResponseDto {
                 .lastName(user.getLastName())
                 .phoneNumber(user.getPhoneNumber())
                 .email(user.getEmail())
-                .role(user.getRole())
+                .role(user.getRoles()
+                        .stream()
+                        .map(Role::getName)
+                        .collect(Collectors.toList()))
                 .address(user.getAddress())
                 .build();
     }
