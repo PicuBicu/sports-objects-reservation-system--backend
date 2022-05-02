87e3e6b748872a91dff375722c40fdf8f5caec99
PicuBicu <championello@gmail.com>
2022-05-02 21:27:38 +0200

    Display unauthorized error and constraint error

--- src/main/java/pl/picubicu/sportsobjectsreservationsystem/custom/ExceptionResolver.java
+++ src/main/java/pl/picubicu/sportsobjectsreservationsystem/custom/ExceptionResolver.java
@@ -5,6 +5,7 @@ import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,6 +19,24 @@ import java.util.Map;
 @Slf4j
 @ControllerAdvice
 public class ExceptionResolver extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException exp) {
+        String message = exp.getMessage();
+        return new ResponseEntity<>(
+                message,
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(value = AuthenticationException.class)
+    public ResponseEntity<Object> handleAuthException(AuthenticationException exp) {
+        String message = exp.getMessage();
+        return new ResponseEntity<>(
+                message,
+                HttpStatus.UNAUTHORIZED
+        );
+    }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exp, HttpHeaders headers, HttpStatus status, WebRequest request) {
