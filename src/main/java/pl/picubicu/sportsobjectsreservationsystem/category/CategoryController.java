ac9999f13e98a0c7b0e0fb4fbdec591d8fe43513
PicuBicu <championello@gmail.com>
2022-05-02 14:42:11 +0200

    Set allowed roles in category controller

--- src/main/java/pl/picubicu/sportsobjectsreservationsystem/category/CategoryController.java
+++ src/main/java/pl/picubicu/sportsobjectsreservationsystem/category/CategoryController.java
@@ -1,5 +1,6 @@
 package pl.picubicu.sportsobjectsreservationsystem.category;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
+@SecurityRequirement(name = "bearerAuth")
 @RequiredArgsConstructor
 @RequestMapping("api/category")
 @RestController
@@ -22,11 +25,13 @@ public class CategoryController {
         return this.categoryService.getAllCategories();
     }
 
+    @RolesAllowed(value = {"ADMIN"})
     @PostMapping()
     public String addCategory(@RequestParam String categoryName) {
         return this.categoryService.addNewCategory(categoryName);
     }
 
+    @RolesAllowed(value = {"ADMIN"})
     @DeleteMapping()
     public String deleteCategory(@RequestParam String categoryName) {
         return this.categoryService.deleteCategory(categoryName);
